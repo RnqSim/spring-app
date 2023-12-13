5b30fb23136ce10b2c654d24d5e330fa5aaf2a85
rencionoel <rencionoel@gmail.com>
2023-12-13 08:07:40 +0800

    updated controller test 2 + app props 3 + appointment controller

--- src/main/java/com/dcc/clinics/service/AppointmentService.java
+++ src/main/java/com/dcc/clinics/service/AppointmentService.java
@@ -81,7 +81,7 @@ public class AppointmentService {
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setHost("https://spring-render-qpn7.onrender.com").build();
         Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("docclickconnect@gmail.com");
         //returns an authorized Credential object.
         return credential;
